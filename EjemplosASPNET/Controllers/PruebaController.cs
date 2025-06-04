ac3829d771dc7bea93e8132d1298c58ddda7830e
WinTux <rusok.software@gmail.com>
2025-06-03 21:24:43 -0400

    Enviando datos al controlador

--- EjemplosASPNET/Controllers/PruebaController.cs
+++ EjemplosASPNET/Controllers/PruebaController.cs
@@ -1,12 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EjemplosASPNET.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EjemplosASPNET.Controllers
 {
+    // http://localhost:8080/Prueba
     public class PruebaController : Controller
     {
+        // http://localhost:8080/Prueba/Index
         public IActionResult Index()
         {
+            /* Ejemplo de archivos estáticos
+             +++++++++++++++++++++++++++++++
+            
+             CSS, JavaScript e imágenes
+            */
             return View();
+        }
+        // http://localhost:8080/Prueba/PasandoDatos
+        public IActionResult PasandoDatos()
+        {
+            ViewBag.edad = 20;
+            ViewBag.nombre = "Pepe Perales";
+            ViewBag.casado = true;
+            ViewBag.peso = 75.5;
+            ViewBag.fechaNacimiento = new DateTime(2005, 7, 30);
+            return View();
+        }
+
+        // http://localhost:8080/Prueba/PasandoObjetos
+        public IActionResult PasandoObjetos()
+        {
+            Producto producto = new Producto()
+            {
+                Id = 1,
+                Nombre = "Queso de lata",
+                Foto = "queso.jpg",
+                Precio = 19.0,
+                Cantidad = 12
+            };
+            ViewBag.producto = producto;
+            return View("PasandoDatos");
         }
     }
 }
---
+++ EjemplosASPNET/Models/Producto.cs
@@ -0,0 +1,22 @@
+﻿namespace EjemplosASPNET.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Foto { get; set; }
+        public double Precio { get; set; }
+        public int Cantidad { get; set; }
+        // Constructor por defecto
+        public Producto() { }
+        // Constructor con parámetros
+        public Producto(int id, string nombre, string foto, double precio, int cantidad)
+        {
+            Id = id;
+            Nombre = nombre;
+            Foto = foto;
+            Precio = precio;
+            Cantidad = cantidad;
+        }
+    }
+}
