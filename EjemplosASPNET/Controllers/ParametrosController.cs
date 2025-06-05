dd0bd65858ecfd28b84403e353da300513abbc4d
WinTux <rusok.software@gmail.com>
2025-06-04 21:26:29 -0400

    Routes alias

---
+++ EjemplosASPNET/Controllers/ParametrosController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EjemplosASPNET.Controllers
+{
+    [Route("param")] // http://localhost:5194/param
+    public class ParametrosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("ej2")]
+        public IActionResult Ejemplo2()
+        {
+            return View();
+        }
+    }
+}
