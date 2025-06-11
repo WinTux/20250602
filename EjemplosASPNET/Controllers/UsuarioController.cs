a26b9e2650797573b21a102c6a845e6308ceb6df
WinTux <rusok.software@gmail.com>
2025-06-10 21:23:40 -0400

    Validación de formularios.

---
+++ EjemplosASPNET/Controllers/UsuarioController.cs
@@ -0,0 +1,21 @@
+﻿using EjemplosASPNET.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EjemplosASPNET.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index", new Usuario());
+        }
+        [HttpPost]
+        public IActionResult Guardar(Usuario u) {
+            if (ModelState.IsValid) {
+                ViewBag.usuario = u;
+                return View("Exito");
+            } else
+                return View("Index");
+        }
+    }
+}
---
+++ EjemplosASPNET/Models/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EjemplosASPNET.Models
+{
+    public class Usuario
+    {
+        [Required]
+        [MinLength(3,ErrorMessage = "El nombre de usuario debe ser de al menos 3 letras.")]
+        [MaxLength(10, ErrorMessage = "El nombre de usuario debe ser de a lo mucho de 10 letras.")]
+        public string nombre { get; set; }
+        [Required]
+        [MinLength(3)]
+        [MaxLength(10)]
+        [RegularExpression("((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})", ErrorMessage = "La contraseña debe contar con al menos una mayúscula, una minúscula, ...")]
+        public string password { get; set; }   
+        [Required]
+        [Range(18,35, ErrorMessage = "El usuario debe ser mayor de edad y hasta los 35 años máximo")]
+        public int edad {  get; set; }
+        [Required]
+        [EmailAddress]
+        public string email { get; set; }
+        [Url]
+        public string pagina { get; set; }
+    }
+}
