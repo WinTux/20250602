db37265852a956c5b183a50d4f695fa1c9d87d42
WinTux <rusok.software@gmail.com>
2025-06-13 21:25:03 -0400

    Layouts y layouts anidados

---
+++ EjemplosASPNET/Controllers/Layouts2Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EjemplosASPNET.Controllers
+{
+    public class Layouts2Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ EjemplosASPNET/Controllers/LayoutsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EjemplosASPNET.Controllers
+{
+    public class LayoutsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Acerca1()
+        {
+            return View();
+        }
+    }
+}
