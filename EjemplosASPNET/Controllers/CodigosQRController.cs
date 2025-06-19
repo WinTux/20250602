f2f6a3576b1d4bd207dafc91b96e470340c4a162
WinTux <rusok.software@gmail.com>
2025-06-18 21:23:59 -0400

    Preparando  ZXing

---
+++ EjemplosASPNET/Controllers/CodigosQRController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EjemplosASPNET.Controllers
+{
+    public class CodigosQRController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
